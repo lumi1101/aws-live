38e8f8f5fa4fdfcf28268935141a4e012e4f4104
Adam Chin <109855669+lumi1101@users.noreply.github.com>
2024-04-16 13:54:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cn2ea8omoi0h.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3273"
+customdb = "employee"
+custombucket = "adamcwk-employee"
+customregion = "us-east-1"
+
